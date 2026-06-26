c5930803ac6a4d5010ed88f657b4f9f0d28d4ac1
viana26 <vianapessoal@gmail.com>
2026-06-26 20:15:14 -0300

    fix: reset para Dashboard ao logar

--- src/App.tsx
+++ src/App.tsx
@@ -195,6 +195,7 @@ export default function App() {
   // HANDLERS
   // =====================================================
   const handleLoginSuccess = () => {
+    setCurrentTab('dashboard');
     setAuthScreen('app');
   };
 
